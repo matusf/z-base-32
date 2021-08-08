417206256da8bb4de79e7e6edd3de67cfb259724
Matus Ferech <matus.ferech@telekom.com>
2021-08-08 21:54:05 +0200

    Add encoder & decoder

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+const ALPHABET: &[u8] = b"ybndrfg8ejkmcpqxot1uwisza345h769";
+const INVERSE_ALPHABET: [i8; 123] = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 18, -1, 25, 26, 27, 30, 29, 7, 31, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, 24, 1, 12, 3, 8, 5, 6, 28, 21, 9, 10, -1, 11, 2, 16, 13, 14, 4, 22, 17, 19, -1, 20, 15, 0,
+    23,
+];
+
+pub fn encode(input: &[u8]) -> String {
+    let mut result = Vec::new();
+    let chunks = input.chunks(5);
+
+    for chunk in chunks {
+        let buf = {
+            let mut buf = [0u8; 5];
+            for (i, &b) in chunk.iter().enumerate() {
+                buf[i] = b;
+            }
+            buf
+        };
+        result.push(ALPHABET[((buf[0] & 0xF8) >> 3) as usize]);
+        result.push(ALPHABET[((buf[0] & 0x07) << 2 | (buf[1] & 0xC0) >> 6) as usize]);
+        result.push(ALPHABET[((buf[1] & 0x3E) >> 1) as usize]);
+        result.push(ALPHABET[((buf[1] & 0x01) << 4 | (buf[2] & 0xF0) >> 4) as usize]);
+        result.push(ALPHABET[((buf[2] & 0x0F) << 1 | (buf[3] & 0x80) >> 7) as usize]);
+        result.push(ALPHABET[((buf[3] & 0x7C) >> 2) as usize]);
+        result.push(ALPHABET[((buf[3] & 0x03) << 3 | (buf[4] & 0xE0) >> 5) as usize]);
+        result.push(ALPHABET[(buf[4] & 0x1F) as usize]);
+    }
+
+    String::from_utf8(result[..(input.len() * 8 / 5 + 1)].to_vec()).unwrap()
+}
+
+pub fn decode(input: &str) -> Option<Vec<u8>> {
+    let mut result = Vec::new();
+    for chunk in input.as_bytes().chunks(8) {
+        let buf = {
+            let mut buf = [0u8; 8];
+            for (i, &ch) in chunk.iter().enumerate() {
+                match INVERSE_ALPHABET.get(ch as usize) {
+                    Some(-1) => return None,
+                    Some(x) => buf[i] = *x as u8,
+                    None => return None,
+                };
+            }
+            buf
+        };
+        result.push((buf[0] << 3) | (buf[1] >> 2));
+        result.push((buf[1] << 6) | (buf[2] << 1) | (buf[3] >> 4));
+        result.push((buf[3] << 4) | (buf[4] >> 1));
+        result.push((buf[4] << 7) | (buf[5] << 2) | (buf[6] >> 3));
+        result.push((buf[6] << 5) | buf[7]);
+    }
+
+    Some(result[..input.len() * 5 / 8].to_vec())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn simple_encode() {
+        assert_eq!(encode(b"asdasd"), "cf3seamuco".to_string());
+    }
+
+    #[test]
+    fn simple_decode() {
+        assert_eq!(decode("cf3seamu"), Some(b"asdas".to_vec()))
+    }
+
+    #[test]
+    fn encode_decode() {
+        assert_eq!(
+            String::from_utf8(decode(&encode(b"foo")).unwrap()).unwrap(),
+            "foo".to_string()
+        )
+    }
+
+    #[test]
+    fn invalid_decode() {
+        assert_eq!(decode("bar#"), None)
+    }
+}
